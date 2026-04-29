04f05c38477b9614539907038ee97a664f97fce5
Purvansh <pvishwakarma@bestpeers.com>
2026-04-30 01:01:33 +0530

    added CI pipeline

--- src/extract.py
+++ src/extract.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def extract():
-    df = pd.read_csv(r"data\Electric_Vehicle_Population_Data.csv")
+    df = pd.read_csv(r"data/Electric_Vehicle_Population_Data.csv")
     return df  
 
 
